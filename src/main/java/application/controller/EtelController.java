7abbdba72261fabc4e2b161aee315a02a4d1c8be
Oberhauser Attila <h879870@stud.u-szeged.hu>
2021-11-05 20:14:31 +0100

    EtelDAO és EtelController

---
+++ src/main/java/application/controller/EtelController.java
@@ -0,0 +1,56 @@
+package application.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import application.dao.EtelDAO;
+import application.model.Etel;
+
+@Controller
+public class EtelController {
+
+	@Autowired
+	private EtelDAO etelDAO;
+
+	@GetMapping("/etelek")
+	public String listEtel(Model model) {
+		model.addAttribute("etelek", etelDAO.listEtelek());
+
+		return "index";
+	}
+
+	 @PostMapping(value = "/addetel")
+	public String addEtel(@RequestParam("nev") String nev, @RequestParam("ar") int ar) {
+		Etel etel = new Etel(nev, ar);
+		etelDAO.insertEtel(etel);
+
+		return "index";
+	}
+
+	@PostMapping(value = "/deleteetel/{nev}")
+	public String deleteEtel(@PathVariable("nev") String nev) {
+		etelDAO.deleteEtel(nev);
+	
+		return "index";
+	}
+
+	@GetMapping(value = "/editetel/{nev}")
+	public String editEtel(@PathVariable("nev") String nev, Model model) {
+		Etel etel = etelDAO.getEtelByNev(nev);
+		model.addAttribute("etel", etel);
+
+		return "update-etel";
+	}
+
+	@PostMapping(value = "/updateetel/{nev}")
+	public String updateEtel(@PathVariable("nev") String nev, @RequestParam("ar") int ar) {
+		etelDAO.updateEtel(nev, ar);
+
+		return "index";
+	} 
+}
---
+++ src/main/java/application/dao/EtelDAO.java
@@ -0,0 +1,75 @@
+package application.dao;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
+import org.springframework.stereotype.Repository;
+
+
+import application.model.Etel;
+
+
+	@Repository
+	public class EtelDAO extends JdbcDaoSupport  {
+		
+		@Autowired 
+		DataSource dataSource;
+		
+		@PostConstruct
+		private void initialize(){
+			setDataSource(dataSource);
+		}
+		
+		 public void insertEtel(Etel etel) {
+			String sql = "INSERT INTO etel(nev, ar) VALUES (?, ?)";
+			getJdbcTemplate().update(sql, new Object[]{
+					etel.getNev(), etel.getAr()
+			});
+		} 
+		
+		public List<Etel> listEtelek(){
+			String sql = "SELECT * FROM etel";
+			List<Map<String, Object>> rows = getJdbcTemplate().queryForList(sql);
+			
+			List<Etel> result = new ArrayList<Etel>();
+			for(Map<String, Object> row:rows){
+				Etel etel = new Etel();
+				etel.setNev((String)row.get("nev"));
+				etel.setAr((Integer)row.get("ar"));
+				result.add(etel);
+			}
+			
+			return result;
+		}
+		
+		public Etel getEtelByNev(String nev){
+			String sql = "SELECT * FROM etel WHERE nev="+nev;
+			List<Map<String, Object>> rows = getJdbcTemplate().queryForList(sql);
+			
+			List<Etel> result = new ArrayList<Etel>();
+			for(Map<String, Object> row:rows){
+				Etel etel = new Etel();
+				etel.setNev((String)row.get("nev"));
+				etel.setAr((Integer)row.get("ar"));
+				result.add(etel);
+			}
+			
+			return result.get(0);
+		}
+		
+		public void deleteEtel(String nev){
+			String sql = "DELETE FROM etel WHERE nev="+nev;
+			getJdbcTemplate().update(sql);	
+		}
+		
+		public void updateEtel(String nev, int ar){
+			String sql = "UPDATE Etel SET nev='"+nev+"', ar='"+ar+"' WHERE nev="+nev;
+			getJdbcTemplate().update(sql);	
+		}
+			
+	
+}
