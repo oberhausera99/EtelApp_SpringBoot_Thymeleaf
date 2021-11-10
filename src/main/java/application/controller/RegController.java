d98ed900edcce12f43183f5fe84b6dd07a7f64df
Nagy György <h049468@stud.u-szeged.hu>
2021-11-10 14:43:20 +0100

    Felesleges fájlok törlése, favicon hozzáadása

--- src/main/java/application/controller/RegController.java
+++ src/main/java/application/controller/RegController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import application.dao.DogDAO;
+
 import application.dao.RegDAO;
 import application.model.Kosar;
 import application.model.User;
